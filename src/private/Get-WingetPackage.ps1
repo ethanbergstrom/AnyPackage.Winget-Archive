9edc55f12c58c8a39ace317a35932bf5b15ea78f
Ethan Bergstrom <13603162+ethanbergstrom@users.noreply.github.com>
2023-02-12 16:26:52 +0000

    Code cleanup

--- src/private/Get-WingetPackage.ps1
+++ src/private/Get-WingetPackage.ps1
@@ -7,7 +7,7 @@ function Get-WingetPackage {
 
 	# Filter results by any name and version requirements
 	# We apply additional package name filtering when using wildcards to make Winget's wildcard behavior more PowerShell-esque
-	Cobalt\Get-WingetPackage @WingetParams |
+	Cobalt\Get-WingetPackage |
 		Where-Object {$Request.IsMatch($_.ID)} |
 			Where-Object {-Not $Request.Version -Or (([NuGet.Versioning.VersionRange]$Request.Version).Satisfies($_.Version))}
 }
